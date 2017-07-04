d662eefae1f27b70f6e4ceec1f25acaf9a497694
Norbert Lugosi <lugosi.norbert@hotmail.com>
2017-07-05 00:34:37 +0200

    copy, move, zip, unzip, open, delete - functions working!

---
+++ SanityArchiver/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace SanityArchiver
+{
+    partial class SanityArchiver
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox13 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox14 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(458, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(23, 120);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(458, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // pictureBox13
+            // 
+            this.pictureBox13.Image = global::SanityArchiver.Properties.Resources.download__1_;
+            this.pictureBox13.Location = new System.Drawing.Point(554, 63);
+            this.pictureBox13.Name = "pictureBox13";
+            this.pictureBox13.Size = new System.Drawing.Size(33, 34);
+            this.pictureBox13.TabIndex = 19;
+            this.pictureBox13.TabStop = false;
+            this.pictureBox13.Visible = false;
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.Image = global::SanityArchiver.Properties.Resources.open_green_folder_icon;
+            this.pictureBox12.Location = new System.Drawing.Point(1036, 326);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(132, 134);
+            this.pictureBox12.TabIndex = 18;
+            this.pictureBox12.TabStop = false;
+            this.pictureBox12.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox12_MouseClick);
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Image = global::SanityArchiver.Properties.Resources._write_line_pencil_128;
+            this.pictureBox11.Location = new System.Drawing.Point(660, 520);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(132, 134);
+            this.pictureBox11.TabIndex = 17;
+            this.pictureBox11.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Image = global::SanityArchiver.Properties.Resources.Read_Only_128;
+            this.pictureBox10.Location = new System.Drawing.Point(660, 326);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(132, 134);
+            this.pictureBox10.TabIndex = 16;
+            this.pictureBox10.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Image = global::SanityArchiver.Properties.Resources.edit_delete_icon;
+            this.pictureBox9.Location = new System.Drawing.Point(1033, 520);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(132, 134);
+            this.pictureBox9.TabIndex = 15;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox9_MouseClick);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::SanityArchiver.Properties.Resources._699371_icon_24_key_128;
+            this.pictureBox8.Location = new System.Drawing.Point(455, 520);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(132, 134);
+            this.pictureBox8.TabIndex = 14;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::SanityArchiver.Properties.Resources._643574_password_512x512;
+            this.pictureBox7.Location = new System.Drawing.Point(455, 326);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(132, 134);
+            this.pictureBox7.TabIndex = 13;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::SanityArchiver.Properties.Resources._5359290;
+            this.pictureBox6.Location = new System.Drawing.Point(238, 520);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(132, 134);
+            this.pictureBox6.TabIndex = 12;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox6_MouseClick);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::SanityArchiver.Properties.Resources.file_move_icone_8684_128;
+            this.pictureBox5.Location = new System.Drawing.Point(23, 520);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(132, 134);
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox5_MouseClick);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::SanityArchiver.Properties.Resources.zip_archieve_compressed_file_format_zip_iconcompressrar;
+            this.pictureBox4.Location = new System.Drawing.Point(238, 326);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(132, 134);
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox4_MouseClick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::SanityArchiver.Properties.Resources.copy;
+            this.pictureBox3.Location = new System.Drawing.Point(23, 326);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(132, 134);
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox3_MouseClick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::SanityArchiver.Properties.Resources.icon_astro;
+            this.pictureBox2.Location = new System.Drawing.Point(487, 109);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(44, 43);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SanityArchiver.Properties.Resources.icon_astro;
+            this.pictureBox1.Location = new System.Drawing.Point(487, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(44, 43);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // pictureBox14
+            // 
+            this.pictureBox14.Image = global::SanityArchiver.Properties.Resources.download__1_;
+            this.pictureBox14.Location = new System.Drawing.Point(554, 118);
+            this.pictureBox14.Name = "pictureBox14";
+            this.pictureBox14.Size = new System.Drawing.Size(33, 34);
+            this.pictureBox14.TabIndex = 20;
+            this.pictureBox14.TabStop = false;
+            this.pictureBox14.Visible = false;
+            // 
+            // SanityArchiver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1188, 665);
+            this.Controls.Add(this.pictureBox14);
+            this.Controls.Add(this.pictureBox13);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "SanityArchiver";
+            this.Text = "SanityArchiver";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.PictureBox pictureBox13;
+        private System.Windows.Forms.PictureBox pictureBox14;
+    }
+}
+
---
+++ SanityArchiver/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.FileIO;
+
+namespace SanityArchiver
+{
+    public partial class SanityArchiver : MetroFramework.Forms.MetroForm
+    {
+        private string selectedFileName,
+            selectedFilePath,
+            compressedDestinationPath,
+            destinationPath,
+            decompressDestinationPath;
+
+
+        private void pictureBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult dr = fbd.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                string destinationPath = fbd.SelectedPath + "\\" + this.selectedFileName;
+                this.destinationPath = destinationPath;
+                textBox2.Text = destinationPath;
+                if (File.Exists(selectedFilePath))
+                {
+                    pictureBox14.Visible = true;
+                }
+            }
+        }
+
+        private void fileCopyWithDialog(string sourcePath, string destinationPath, string message)
+        {
+            sourcePath = selectedFilePath;
+            destinationPath = this.destinationPath;
+            FileSystem.CopyFile(sourcePath, destinationPath,
+                UIOption.AllDialogs);
+            if (File.Exists(destinationPath))
+                MessageBox.Show(message);
+        }
+
+        private void pictureBox3_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (textBox1.Text != null && textBox2.Text != null && textBox1.Text == selectedFileName &&
+                textBox2.Text == destinationPath)
+            {
+                if (File.Exists(destinationPath))
+                {
+                    File.Delete(destinationPath);
+                    fileCopyWithDialog(selectedFilePath, destinationPath, "Copy Success!");
+                }
+
+                else
+                {
+                    fileCopyWithDialog(selectedFilePath, destinationPath, "Copy Success!");
+                }
+            }
+        }
+
+
+        private void pictureBox9_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (File.Exists(selectedFilePath))
+            {
+                FileSystem.DeleteFile(selectedFilePath, UIOption.AllDialogs, RecycleOption.SendToRecycleBin);
+                if (!File.Exists(selectedFilePath))
+                    MessageBox.Show("Delete Success!");
+            }
+        }
+
+        private void pictureBox5_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (textBox1.Text != null && textBox2.Text != null && textBox1.Text == selectedFileName &&
+                textBox2.Text == destinationPath)
+            {
+                if (File.Exists(destinationPath))
+                {
+                    File.Delete(destinationPath);
+                    fileCopyWithDialog(selectedFilePath, destinationPath, "Move Success!");
+                    FileSystem.DeleteFile(selectedFilePath, UIOption.AllDialogs, RecycleOption.SendToRecycleBin);
+                }
+
+                else
+                {
+                    fileCopyWithDialog(selectedFilePath, destinationPath, "Move Success!");
+                    FileSystem.DeleteFile(selectedFilePath, UIOption.AllDialogs, RecycleOption.SendToRecycleBin);
+                }
+            }
+        }
+
+        private void pictureBox12_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (textBox1.Text != null && textBox1.Text == selectedFileName)
+                Process.Start(selectedFilePath);
+        }
+
+        private void pictureBox4_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!File.Exists(compressedDestinationPath) && textBox2.Text != null && textBox2.Text == destinationPath &&
+                textBox1.Text != null && textBox1.Text == selectedFileName)
+            {
+                doCompress();
+            }
+            else
+            {
+                if (File.Exists(compressedDestinationPath))
+                {
+                    File.Delete(compressedDestinationPath);
+                    doCompress();
+                }
+            }
+        }
+
+        void doDecompress()
+        {
+            var token = destinationPath.ToList();
+            string decompressedDestination = "";
+            for (int i = 0; i < token.Count - 3; i++)
+            {
+                decompressedDestination += token[i];
+            }
+
+            using (FileStream fs = File.OpenRead(selectedFilePath))
+            using (FileStream fw = File.Create(decompressedDestination))
+
+            using (GZipStream zipStream = new GZipStream(fs, CompressionMode.Decompress, false))
+            {
+                byte[] buffer = new byte[1024];
+                int nRead;
+                while ((nRead = zipStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    fw.Write(buffer, 0, nRead);
+                }
+            }
+            MessageBox.Show("Decompress Success!");
+        }
+
+        void doCompress()
+        {
+            compressedDestinationPath = destinationPath + ".gz";
+            var bytes = File.ReadAllBytes(selectedFilePath);
+            using (FileStream fs = new FileStream(compressedDestinationPath, FileMode.CreateNew))
+            using (GZipStream zipStream = new GZipStream(fs, CompressionMode.Compress, false))
+            {
+                zipStream.Write(bytes, 0, bytes.Length);
+            }
+            MessageBox.Show("Compress Success!");
+        }
+
+        private void pictureBox6_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (destinationPath != null)
+            {
+                var token = destinationPath.ToList();
+                string decompressedDestination = "";
+                for (int i = 0; i < token.Count - 3; i++)
+                {
+                    decompressedDestination += token[i];
+                }
+                if (!File.Exists(decompressedDestination))
+                    if (textBox1.Text != "Plase select the file u'd like to work with !" &&
+                        textBox2.Text != "Please select the destination folder if needed !")
+                        doDecompress();
+                    else
+                    {
+                        File.Delete(decompressedDestination);
+                        doDecompress();
+
+                    }
+            }
+        }
+
+        public SanityArchiver()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "Plase select the file u'd like to work with !";
+            textBox2.Text = "Please select the destination folder if needed !";
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            DialogResult dr = ofd.ShowDialog();
+
+            if (dr == DialogResult.OK)
+            {
+                string selectedFilePath = ofd.FileName;
+                string selectedFileName = ofd.SafeFileName;
+                this.selectedFilePath = selectedFilePath;
+                this.selectedFileName = selectedFileName;
+                textBox1.Text = selectedFileName;
+                if (File.Exists(selectedFilePath))
+                {
+                    pictureBox13.Visible = true;
+                }
+            }
+        }
+    }
+}
